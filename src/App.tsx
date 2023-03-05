6446f945e74dc52d930223553565b2dd86eaf1e0
ctnkaan <kaan.taskingenc@gmail.com>
2023-03-06 02:46:31 +0300

    :sparkles: Left Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Login from "./routes/Login";
 import NotFound from "./routes/NotFound";
 import Root from "./routes/Root";
+import LeftNavbar from "./components/LeftNavbar/LeftNavbar";
 
 function App() {
-	const isAuthenticated = false; // Replace with your authentication logic
+	const isAuthenticated = true; // Replace with your authentication logic
+
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -15,11 +17,21 @@ function App() {
 	}, []);
 
 	return (
-		<Routes>
-			<Route path="/" element={<Root />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+		<div
+			style={{
+				display: "flex",
+				height: "100vh",
+				width: "100vw",
+			}}
+		>
+			{isAuthenticated && <LeftNavbar />}
+
+			<Routes>
+				<Route path="/" element={<Root />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</div>
 	);
 }
 
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LeftNavbar from "../components/LeftNavbar/LeftNavbar";
 
 const Root = () => {
 	return <div>Root</div>;
